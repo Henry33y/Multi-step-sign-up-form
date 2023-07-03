e8bc06f3cc5c7575c7ac579c28475fe77273381b
Henry <henryasante336@gmail.com>
2023-07-03 16:26:34 +0000

    Inital Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const form1 = document.querySelector('.form1')
+const form2 = document.querySelector('.form2')
+const form3 = document.querySelector('.form3')
+
+const next1 = document.querySelector('.next1')
+const next2 = document.querySelector('.next2')
+const back2 = document.querySelector('.back2')
+const back3 = document.querySelector('.back3')
+
+const progress = document.querySelector('.myProgress')
+
+next1.addEventListener('click',()=>{
+    form1.style.left='-450px'
+    form2.style.left='40px'
+    form3.style.left='450px'
+    progress.style.width='240px'
+    console.log("next 1 clicked")
+})
+back2.addEventListener('click',()=>{
+    form1.style.left='40px'
+    form2.style.left='450px'
+    form3.style.left='900px'
+    progress.style.width='120px'
+    console.log("back 2 clicked")
+})
+next2.addEventListener('click',()=>{
+    form1.style.left='-900px'
+    form2.style.left='-450px'
+    form3.style.left='40px'
+    progress.style.width='400px'
+    console.log("next 2 clicked")
+})
+back3.addEventListener('click',()=>{
+    form1.style.left='-450px'
+    form2.style.left='40px'
+    form3.style.left='450px'
+    progress.style.width='240px'
+    console.log("back 2 clicked")
+})
